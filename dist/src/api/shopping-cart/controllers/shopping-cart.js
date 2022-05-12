18810a00f75c05f4f237777fb0302f9f3ad31eb3
MaksZhukov <maks_zhukov_97@mail.ru>
2022-05-12 16:34:59 +0300

    Added deleting items from cart

--- dist/src/api/shopping-cart/controllers/shopping-cart.js
+++ dist/src/api/shopping-cart/controllers/shopping-cart.js
@@ -4,21 +4,39 @@
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController("api::shopping-cart.shopping-cart", 
 // @ts-ignore
-exports.default = strapi_1.factories.createCoreController('api::shopping-cart.shopping-cart', () => ({
+({ strapi }) => ({
     async find(ctx) {
         const userId = ctx.state.user.id;
         ctx.query = {
-            populate: 'products',
+            populate: "product",
             filters: {
                 users_permissions_user: {
                     id: {
-                        $eq: userId
-                    }
-                }
-            }
+                        $eq: userId,
+                    },
+                },
+            },
         };
-        let { data } = await super.find(ctx);
-        return { data: data[0], meta: {} };
-    }
+        return await super.find(ctx);
+    },
+    async delete(ctx) {
+        const { id } = ctx.params;
+        const userId = ctx.state.user.id;
+        if (await strapi.db.query("api::shopping-cart.shopping-cart").findOne({
+            where: {
+                id: {
+                    $eq: id,
+                },
+                users_permissions_user: {
+                    id: {
+                        $eq: userId,
+                    },
+                },
+            },
+        })) {
+            return await super.delete(ctx);
+        }
+    },
 }));
--- src/api/shopping-cart/controllers/shopping-cart.ts
+++ src/api/shopping-cart/controllers/shopping-cart.ts
@@ -2,23 +2,46 @@
  *  shopping-cart controller
  */
 
+import { factories } from "@strapi/strapi";
 
-import { factories } from '@strapi/strapi'
-// @ts-ignore
-export default factories.createCoreController('api::shopping-cart.shopping-cart', () => ({
+export default factories.createCoreController(
+  "api::shopping-cart.shopping-cart",
+  // @ts-ignore
+  ({ strapi }) => ({
     async find(ctx) {
-        const userId = ctx.state.user.id;
-        ctx.query = {
-            populate: 'products',
-            filters: {
-                users_permissions_user: {
-                    id: {
-                        $eq: userId
-                    }
-                }
-            }
-        };
-        let { data } = await super.find(ctx);
-        return { data: data[0], meta: {} };
-    }
-}));
+      const userId = ctx.state.user.id;
+      ctx.query = {
+        populate: "product",
+        filters: {
+          users_permissions_user: {
+            id: {
+              $eq: userId,
+            },
+          },
+        },
+      };
+      return await super.find(ctx);
+    },
+
+    async delete(ctx) {
+      const { id } = ctx.params;
+      const userId = ctx.state.user.id;
+      if (
+        await strapi.db.query("api::shopping-cart.shopping-cart").findOne({
+          where: {
+            id: {
+              $eq: id,
+            },
+            users_permissions_user: {
+              id: {
+                $eq: userId,
+              },
+            },
+          },
+        })
+      ) {
+        return await super.delete(ctx);
+      }
+    },
+  })
+);
