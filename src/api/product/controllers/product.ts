8e69b5b9f36b8e16dca8e98f988939fc919825c9
MaksZhukov <maks_zhukov_97@mail.ru>
2022-05-16 19:28:59 +0300

    Added slug functionality

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -2,6 +2,19 @@
  *  product controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::product.product');
+export default factories.createCoreController(
+  "api::product.product",
+  //@ts-ignore
+  ({ strapi }) => ({
+    async findOne(ctx) {
+      const { id } = ctx.params;
+      const entity = await strapi.db.query("api::product.product").findOne({
+        where: { $or: [{ slug: id }, { id }] },
+      });
+      const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+      return this.transformResponse(sanitizedEntity);
+    },
+  })
+);
--- src/api/shopping-cart/controllers/shopping-cart.ts
+++ src/api/shopping-cart/controllers/shopping-cart.ts
@@ -5,43 +5,43 @@
 import { factories } from "@strapi/strapi";
 
 export default factories.createCoreController(
-  "api::shopping-cart.shopping-cart",
-  // @ts-ignore
-  ({ strapi }) => ({
-    async find(ctx) {
-      const userId = ctx.state.user.id;
-      ctx.query = {
-        populate: "product",
-        filters: {
-          users_permissions_user: {
-            id: {
-              $eq: userId,
-            },
-          },
+    "api::shopping-cart.shopping-cart",
+    // @ts-ignore
+    ({ strapi }) => ({
+        async find(ctx) {
+            const userId = ctx.state.user.id;
+            ctx.query = {
+                populate: "product",
+                filters: {
+                    users_permissions_user: {
+                        id: {
+                            $eq: userId,
+                        },
+                    },
+                },
+            };
+            return await super.find(ctx);
         },
-      };
-      return await super.find(ctx);
-    },
 
-    async delete(ctx) {
-      const { id } = ctx.params;
-      const userId = ctx.state.user.id;
-      if (
-        await strapi.db.query("api::shopping-cart.shopping-cart").findOne({
-          where: {
-            id: {
-              $eq: id,
-            },
-            users_permissions_user: {
-              id: {
-                $eq: userId,
-              },
-            },
-          },
-        })
-      ) {
-        return await super.delete(ctx);
-      }
-    },
-  })
+        async delete(ctx) {
+            const { id } = ctx.params;
+            const userId = ctx.state.user.id;
+            if (
+                await strapi.db.query("api::shopping-cart.shopping-cart").findOne({
+                    where: {
+                        id: {
+                            $eq: id,
+                        },
+                        users_permissions_user: {
+                            id: {
+                                $eq: userId,
+                            },
+                        },
+                    },
+                })
+            ) {
+                return await super.delete(ctx);
+            }
+        },
+    })
 );
