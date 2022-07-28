76b94e4c072c0c42d7206c3311dbcc237f0ed2e0
Maksim Zhukau <maksimz@checkpoint.com>
2022-07-28 16:59:41 +0300

    Fixed getting currency error

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -1,7 +1,7 @@
 /**
  *  product controller
  */
-
+import { Agent } from "https";
 import { factories } from "@strapi/strapi";
 import axios from "axios";
 
@@ -9,19 +9,25 @@ let coefficient = 0;
 
 const fetchCoefficient = async () => {
     try {
-        const { data: { rates: { BYN } } } = await axios.get('https://api.currencyfreaks.com/latest?apikey=77c86f7878774b21b8edf00dbc45d550&symbols=BYN');
+        const {
+            data: {
+                rates: { BYN },
+            },
+        } = await axios.get(
+            "https://api.currencyfreaks.com/latest?apikey=77c86f7878774b21b8edf00dbc45d550&symbols=BYN",
+            { httpsAgent: new Agent({ rejectUnauthorized: false }) }
+        );
         coefficient = 1 / BYN;
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-}
+};
 
 fetchCoefficient();
 
 setInterval(() => {
     fetchCoefficient();
-}, 60 * 60 * 1000)
-
+}, 60 * 60 * 1000);
 
 export default factories.createCoreController(
     "api::product.product",
@@ -32,7 +38,12 @@ export default factories.createCoreController(
                 .query("api::product.product")
                 .findOne({
                     where: { $or: [{ slug: id }, { id }] },
-                    populate: { images: true, sparePart: true, model: true, brand: true },
+                    populate: {
+                        images: true,
+                        sparePart: true,
+                        model: true,
+                        brand: true,
+                    },
                 });
             entity.priceUSD = entity.price * coefficient;
             const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
@@ -40,12 +51,16 @@ export default factories.createCoreController(
         },
         async find(ctx) {
             const response = await super.find(ctx);
-            response.data = await Promise.all(response.data.map(async item => ({
-                ...item, attributes: {
-                    ...item.attributes, priceUSD: item.attributes.price * coefficient
-                }
-            })))
+            response.data = await Promise.all(
+                response.data.map(async (item) => ({
+                    ...item,
+                    attributes: {
+                        ...item.attributes,
+                        priceUSD: item.attributes.price * coefficient,
+                    },
+                }))
+            );
             return response;
-        }
+        },
     })
 );
