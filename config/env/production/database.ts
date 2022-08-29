73a3ab55d3b45a0014f97cd440f4c9a5be40865a
MaksZhukov <maks_zhukov_97@mail.ru>
2022-08-29 20:52:31 +0300

    Removed unsecured data

--- config/env/production/database.ts
+++ config/env/production/database.ts
@@ -4,9 +4,9 @@ export default ({ env }) => ({
         connection: {
             host: env("DATABASE_HOST", "127.0.0.1"),
             port: env.int("DATABASE_PORT", 3306),
-            database: env("DATABASE_NAME", "razbor_auto"),
-            user: env("DATABASE_USERNAME", "razbor_auto"),
-            password: env("DATABASE_PASSWORD", "TxH#CUcL29Y7HvzS"),
+            database: env("DATABASE_NAME"),
+            user: env("DATABASE_USERNAME"),
+            password: env("DATABASE_PASSWORD"),
             ssl: env.bool("DATABASE_SSL", true),
             multipleStatements: true,
         },
