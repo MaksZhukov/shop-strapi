7bdd66491231d8d9093c74a3a4fcb40330508d73
MaksZhukov <maks_zhukov_97@mail.ru>
2022-09-05 10:09:07 +0300

    Added Car API

--- src/api/car/controllers/car.ts
+++ src/api/car/controllers/car.ts
@@ -2,6 +2,18 @@
  *  car controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::car.car');
+export default factories.createCoreController("api::car.car", ({ strapi }) => ({
+    async findOne(ctx) {
+        const { id } = ctx.params;
+        const entity = await strapi.db.query("api::car.car").findOne({
+            where: { $or: [{ slug: id }, { id }] },
+            populate: ["images", "model", "brand"],
+        });
+        if (entity) {
+            const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+            return this.transformResponse(sanitizedEntity);
+        }
+    },
+}));
