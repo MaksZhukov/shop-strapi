6eaed3d16c89da237ca74b66501552b3bfa04bbc
MaksZhukov <maks_zhukov_97@mail.ru>
2022-10-09 15:36:47 +0300

    Fixed cache for shopping cart

--- src/api/shopping-cart/routes/custom-shopping-cart.ts
+++ src/api/shopping-cart/routes/custom-shopping-cart.ts
@@ -7,22 +7,22 @@ export default {
         {
             method: "GET",
             path: "/shopping-cart",
-            handler: "shopping-cart.find",
+            handler: "api::shopping-cart.shopping-cart.find",
         },
         {
             method: "POST",
             path: "/shopping-cart",
-            handler: "shopping-cart.create",
+            handler: "api::shopping-cart.shopping-cart.create",
         },
         {
             method: "DELETE",
             path: "/shopping-cart",
-            handler: "shopping-cart.deleteMany",
+            handler: "api::shopping-cart.shopping-cart.deleteMany",
         },
         {
             method: "DELETE",
             path: "/shopping-cart/:id",
-            handler: "shopping-cart.delete",
+            handler: "api::shopping-cart.shopping-cart.delete",
         },
     ],
 };
