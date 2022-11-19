920f3cad22f701bef6991bd263b6ca748fb2bdf3
MaksZhukov <maks_zhukov_97@mail.ru>
2022-11-19 21:04:24 +0300

    Added snippets for product and for product page

---
+++ src/api/page-product/routes/page-product.ts
@@ -0,0 +1,7 @@
+/**
+ * page-product router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::page-product.page-product');
--- src/api/tire/controllers/tire.ts
+++ src/api/tire/controllers/tire.ts
@@ -11,7 +11,12 @@ export default factories.createCoreController(
             const { id } = ctx.params;
             const entity = await strapi.db.query("api::tire.tire").findOne({
                 where: { $or: [{ slug: id }, { id }] },
-                populate: ["images", "brand", "seo.images"],
+                populate: [
+                    "images",
+                    "brand",
+                    "seo.images",
+                    "snippets.benefits",
+                ],
             });
             return this.transformResponse(entity);
         },
