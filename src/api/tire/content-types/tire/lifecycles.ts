ee45d821f5a8aba61a9fab4518b0cbcbdf2aa155
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-03 12:59:47 +0300

    Added h1 according to data.name

--- src/api/tire/content-types/tire/lifecycles.ts
+++ src/api/tire/content-types/tire/lifecycles.ts
@@ -1,18 +1,6 @@
-import slugify from "slugify";
-import { afterDeleteProduct } from "../../../../lifecycles";
+import { beforeCreateProduct } from "../../../../lifecycles";
 
 export default {
-    beforeCreate(event) {
-        const { data } = event.params;
-        if (data.id && data.name) {
-            data.slug = slugify(data.name, { lower: true }) + "-" + data.id;
-        }
-    },
-    beforeUpdate(event) {
-        const { data } = event.params;
-        if (data.id && data.name) {
-            data.slug = slugify(data.name, { lower: true }) + "-" + data.id;
-        }
-    },
+    beforeCreate: beforeCreateProduct,
     // afterDelete: afterDeleteProduct,
 };
