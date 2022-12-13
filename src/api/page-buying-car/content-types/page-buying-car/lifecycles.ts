10475e5bbb744801997ce6f909fa93e33444323c
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-13 20:22:08 +0300

    Added revalidation for autocomises and service stations

--- src/api/page-buying-car/content-types/page-buying-car/lifecycles.ts
+++ src/api/page-buying-car/content-types/page-buying-car/lifecycles.ts
@@ -1,11 +1,7 @@
+import { revalidateClientPage } from "../../../../lifecycles";
+
 export default {
     async afterUpdate() {
-        try {
-            await strapi
-                .service("api::client.client")
-                .revalidatePage("/buying-car");
-        } catch (err) {
-            console.error(err);
-        }
+        revalidateClientPage("/buying-car");
     },
 };
