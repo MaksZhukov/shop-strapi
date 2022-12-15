1a3aa5773c8485402bfad46bd308afefef1ef2e2
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-15 19:12:29 +0300

    Added url for brands

--- src/api/tire-brand/controllers/tire-brand.ts
+++ src/api/tire-brand/controllers/tire-brand.ts
@@ -2,6 +2,21 @@
  *  tire-brand controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::tire-brand.tire-brand');
+export default factories.createCoreController(
+    "api::tire-brand.tire-brand",
+    ({ strapi }) => ({
+        async findOne(ctx) {
+            const { id } = ctx.params;
+            const { populate } = ctx.query;
+            const entity = await strapi.db
+                .query("api::tire-brand.tire-brand")
+                .findOne({
+                    where: { $or: [{ name: id }, { id }] },
+                    populate: populate,
+                });
+            return this.transformResponse(entity);
+        },
+    })
+);
