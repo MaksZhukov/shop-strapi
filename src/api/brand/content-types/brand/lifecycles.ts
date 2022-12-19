3329c28b6da046d948cb660d96dd4bb376748325
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-20 00:02:49 +0300

    Added lifecycles for brands

---
+++ src/api/brand/content-types/brand/lifecycles.ts
@@ -0,0 +1,10 @@
+import slugify from "slugify";
+
+export default {
+    beforeCreate(event) {
+        const { data } = event.params;
+        if (data.name) {
+            data.slug = slugify(data.name, { lower: true });
+        }
+    },
+};
