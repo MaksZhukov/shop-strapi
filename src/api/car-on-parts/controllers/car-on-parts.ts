e013c3a6053f628f04706dfc4547a4f05133797b
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-19 14:00:32 +0300

    Added search by slug for car on parts

--- src/api/car-on-parts/controllers/car-on-parts.ts
+++ src/api/car-on-parts/controllers/car-on-parts.ts
@@ -2,6 +2,29 @@
  * car-on-parts controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::car-on-parts.car-on-parts');
+export default factories.createCoreController(
+    "api::car-on-parts.car-on-parts",
+    ({ strapi }) => ({
+        async findOne(ctx) {
+            const { id } = ctx.params;
+            const entity = await strapi.db
+                .query("api::car-on-parts.car-on-parts")
+                .findOne({
+                    where: { $or: [{ slug: id }, { id }] },
+                    populate: [
+                        "images",
+                        "model",
+                        "brand",
+                        "generation",
+                        "seo.images",
+                    ],
+                });
+            if (entity) {
+                const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+                return this.transformResponse(sanitizedEntity);
+            }
+        },
+    })
+);
