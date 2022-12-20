e646c8219744f1c8b6f05e78541b08995d0c1d24
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-20 15:59:36 +0300

    Added middleware for sending errors to email

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,4 +10,5 @@ export default [
     "strapi::favicon",
     "strapi::public",
     "global::rate-limit",
+    "global::catching-errors",
 ];
---
+++ src/middlewares/catching-errors.ts
@@ -0,0 +1,15 @@
+export default (config, { strapi }) => {
+    return async (context, next) => {
+        try {
+            await next();
+        } catch (err) {
+            console.log(err);
+            strapi.plugins.email.services.email.send({
+                to: "maks_zhukov_97@mail.ru",
+                from: strapi.plugins.email.config("providerOptions.username"),
+                subject: "Razbor Auto Error",
+                text: err.toString(),
+            });
+        }
+    };
+};
