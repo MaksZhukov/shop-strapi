fbc339aeb3c68e9b83fb6783c014fdf7ef347d3b
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-20 17:28:06 +0300

    Added tire widths, heights, diameters as collection for tires

---
+++ src/api/tire-diameter/routes/tire-diameter.ts
@@ -0,0 +1,7 @@
+/**
+ * tire-diameter router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tire-diameter.tire-diameter');
---
+++ src/api/tire-height/controllers/tire-height.ts
@@ -0,0 +1,7 @@
+/**
+ * tire-height controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::tire-height.tire-height');
