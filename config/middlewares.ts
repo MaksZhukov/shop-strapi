0c4ec7331ec6445b85b9d90ed8bdbd84ea210cfc
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-21 11:10:20 +0300

    Fixed middelware apply

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -10,7 +10,7 @@ export default [
     "strapi::favicon",
     "strapi::public",
     "global::rate-limit",
-    ...(process.env.NODE_ENV === "proruction"
+    ...(process.env.NODE_ENV === "production"
         ? ["global::catching-errors"]
         : []),
 ];
