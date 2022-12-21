9d0a9305e3f66e9cca9b31376e66767dd9da295d
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-21 19:39:35 +0300

    Added relation collections for wheels

---
+++ src/api/wheel-disk-offset/routes/wheel-disk-offset.ts
@@ -0,0 +1,7 @@
+/**
+ * wheel-disk-offset router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::wheel-disk-offset.wheel-disk-offset');
---
+++ src/api/wheel-distance-between-center/routes/wheel-distance-between-center.ts
@@ -0,0 +1,7 @@
+/**
+ * wheel-distance-between-center router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::wheel-distance-between-center.wheel-distance-between-center');
