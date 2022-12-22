fd636c6b057f61be5b6a0ab0c271f5bbb073b1be
MaksZhukov <maks_zhukov_97@mail.ru>
2022-12-22 23:46:29 +0300

    Removed relation distanceBetweenCenters

--- src/api/wheel-distance-between-center/services/wheel-distance-between-center.ts
+++
@@ -1,7 +0,0 @@
-/**
- * wheel-distance-between-center service
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreService('api::wheel-distance-between-center.wheel-distance-between-center');
