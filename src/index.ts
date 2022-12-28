7368d9fd89da01589580315259568581297c9f86
Maksim Zhukau <maksimz@checkpoint.com>
2022-12-28 18:00:25 +0300

    Fixed

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,61 @@ export default {
             process.env.NODE_ENV === "production" &&
             process.env.NODE_APP_INSTANCE === "0"
         ) {
+            const brands = await strapi.db.query("api::brand.brand").findMany();
+            const tireBrands = await strapi.db
+                .query("api::tire-brand.tire-brand")
+                .findMany();
+            let clientUrl = strapi.config.get("server.clientUrl");
+
+            brands.forEach(async (item) => {
+                strapi.entityService.update("api::brand.brand", item.id, {
+                    data: {
+                        ...item,
+                        productBrandTexts: {
+                            sparePartBrandText: {
+                                content: `<p>
+                    Еще больше качественных товаров в категории сайта <a href="${clientUrl}/spare-parts/${
+                                    item.slug
+                                }"><span style="font-family:&quot;Calibri&quot;,sans-serif;">Запчасти для ${item.name.toLowerCase()}</span></a>
+                </p>`,
+                            },
+                            cabinTextBrand: {
+                                content: `<p>
+                    Еще больше качественных товаров в категории сайта <a href="${clientUrl}/cabins/${
+                                    item.slug
+                                }"><span style="font-family:&quot;Calibri&quot;,sans-serif;">Салоны для ${item.name.toLowerCase()}</span></a>
+                </p>`,
+                            },
+                            wheelTextBrand: {
+                                content: `<p>
+                    Еще больше качественных товаров в категории сайта <a href="${clientUrl}/wheels/${
+                                    item.slug
+                                }"><span style="font-family:&quot;Calibri&quot;,sans-serif;">Диски для ${item.name.toLowerCase()}</span></a>
+                </p>`,
+                            },
+                        },
+                    },
+                });
+            });
+
+            tireBrands.forEach(async (item) => {
+                strapi.entityService.update(
+                    "api::tire-brand.tire-brand",
+                    item.id,
+                    {
+                        data: {
+                            ...item,
+                            productBrandText: {
+                                content: `<p>
+                        Еще больше качественных товаров в категории сайта <a href="${clientUrl}/tires/${
+                                    item.slug
+                                }"><span style="font-family:&quot;Calibri&quot;,sans-serif;">Диски для ${item.name.toLowerCase()}</span></a>
+                    </p>`,
+                            },
+                        },
+                    }
+                );
+            });
             if (await hasDelayOfSendingNewProductsEmail(strapi)) {
                 sendNewProductsToEmail({ strapi });
             }
