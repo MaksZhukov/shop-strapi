0f5bdf8395be5ae38235e38ef12c0139ff06dbc0
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-01-28 23:44:30 +0300

    Added updating slugs for models

---
+++ src/api/model/content-types/model/lifecycles.ts
@@ -0,0 +1,17 @@
+import slugify from "slugify";
+import { lifecycleSitemap } from "../../../../lifecycles";
+import { generateDefaultBrandTextComponent } from "../../../../services";
+
+export default {
+    beforeCreate(event) {
+        const { data } = event.params;
+        if (data.name) {
+            data.slug = slugify(data.name, { lower: true });
+        }
+    },
+    afterCreate: (event) => {
+        lifecycleSitemap();
+    },
+    afterUpdate: lifecycleSitemap,
+    afterDelete: lifecycleSitemap,
+};
