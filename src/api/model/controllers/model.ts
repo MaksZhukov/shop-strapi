024516400d01167c5b50c8582f56cf76a168fe75
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-01-29 00:22:56 +0300

    Added ability to find model by slug

--- src/api/model/controllers/model.ts
+++ src/api/model/controllers/model.ts
@@ -2,6 +2,19 @@
  *  model controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::model.model');
+export default factories.createCoreController(
+    "api::model.model",
+    ({ strapi }) => ({
+        async findOne(ctx) {
+            const { id } = ctx.params;
+            const { populate } = ctx.query;
+            const entity = await strapi.db.query("api::model.model").findOne({
+                where: { $or: [{ slug: id }, { id }] },
+                populate: populate,
+            });
+            return this.transformResponse(entity);
+        },
+    })
+);
