ccc368f97a33b52d9d4d8d33fc03bbfa66ba8dd7
Maksim Zhukau <maksimz@checkpoint.com>
2023-02-06 16:41:46 +0300

    Added template for alts for product images

--- src/api/cabin/content-types/cabin/lifecycles.ts
+++ src/api/cabin/content-types/cabin/lifecycles.ts
@@ -1,11 +1,21 @@
-import { beforeCreateProduct, lifecycleSitemap } from "../../../../lifecycles";
+import {
+    afterCreateProduct,
+    beforeCreateProduct,
+    lifecycleSitemap,
+} from "../../../../lifecycles";
 
 // import { afterDeleteProduct } from "../../../../lifecycles";
 
 export default {
     beforeCreate: beforeCreateProduct,
-    afterCreate: lifecycleSitemap,
-    afterUpdate: lifecycleSitemap,
+    afterCreate: (data) => {
+        afterCreateProduct(data);
+        lifecycleSitemap();
+    },
+    afterUpdate: (data) => {
+        afterCreateProduct(data);
+        lifecycleSitemap();
+    },
     afterDelete: lifecycleSitemap,
     // afterDelete: afterDeleteProduct,
 };
