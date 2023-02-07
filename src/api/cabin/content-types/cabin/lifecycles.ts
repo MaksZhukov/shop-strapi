bd16b07568c2d4da9afc949739cfbddcbfbde72a
Maksim Zhukau <maksimz@checkpoint.com>
2023-02-07 16:10:22 +0300

    Fixed

--- src/api/cabin/content-types/cabin/lifecycles.ts
+++ src/api/cabin/content-types/cabin/lifecycles.ts
@@ -9,11 +9,9 @@ import { updateAltTextForProductImages } from "../../../../services";
 export default {
     beforeCreate: beforeCreateProduct,
     afterCreate: (data) => {
-        updateAltTextForProductImages(data.result);
         lifecycleSitemap();
     },
     afterUpdate: (data) => {
-        updateAltTextForProductImages(data.result);
         lifecycleSitemap();
     },
     afterDelete: lifecycleSitemap,
