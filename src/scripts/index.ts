6bd518331114eb3ca432bbc0c1b4c3186d041836
MaksZhukov <maks_zhukov_97@mail.ru>
2023-02-13 23:57:48 +0300

    Revert "add initial alt updates"

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,21 +1,3 @@
-import { updateAltTextForProductImages } from "../services";
-
-const runScripts = async (strapi) => {
-    const spareParts = await strapi.db
-        .query("api::spare-part.spare-part")
-        .findMany({ populate: ["images", "seo"] });
-    const cabins = await strapi.db
-        .query("api::cabin.cabin")
-        .findMany({ populate: ["images", "seo"] });
-    const wheels = await strapi.db
-        .query("api::wheel.wheel")
-        .findMany({ populate: ["images", "seo"] });
-    const tires = await strapi.db
-        .query("api::tire.tire")
-        .findMany({ populate: ["images", "seo"] });
-    [...spareParts, ...cabins, ...wheels, ...tires].forEach((item) => {
-        updateAltTextForProductImages(item, item.images);
-    });
-};
+const runScripts = async (strapi) => {};
 
 export default runScripts;
