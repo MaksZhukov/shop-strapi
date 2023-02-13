8a4425fc692e5bba8cef24c18135037fa74bc7d5
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-02-13 23:37:26 +0300

    Added scedule update alt for tire

--- src/api/tire/content-types/tire/lifecycles.ts
+++ src/api/tire/content-types/tire/lifecycles.ts
@@ -4,11 +4,11 @@ import { scheduleUpdateAltTextForProductImages } from "../../../../services";
 export default {
     beforeCreate: beforeCreateProduct,
     afterCreate: (data) => {
-        scheduleUpdateAltTextForProductImages(data.result, "api::wheel.wheel");
+        scheduleUpdateAltTextForProductImages(data.result, "api:tire.tire");
         lifecycleSitemap();
     },
     afterUpdate: (data) => {
-        scheduleUpdateAltTextForProductImages(data.result, "api::wheel.wheel");
+        scheduleUpdateAltTextForProductImages(data.result, "api::tire.tire");
         lifecycleSitemap();
     },
     afterDelete: lifecycleSitemap,
