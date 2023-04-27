ff61f0f1241d78b8574aeab512d6081baa33e795
Maksim Zhukau <maksimz@checkpoint.com>
2023-04-27 12:48:41 +0300

    Add h1 for wheels

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,21 @@
 const runScripts = async (strapi) => {
-
+    const wheels = await strapi.db
+        .query("api::wheel.wheel")
+        .findMany({ populate: ["brand", "model"] });
+    wheels
+        .filter((item) => item.brand && item.model)
+        .forEach((item) => {
+            strapi.entityService.update("api::wheel.wheel", item.id, {
+                data: {
+                    h1:
+                        item.name +
+                        " " +
+                        item.brand.name +
+                        " " +
+                        item.model.name,
+                },
+            });
+        });
 };
 
 export default runScripts;
