121e42f1f5b89aa8a594e9e79fd8455e1ff91cfc
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-04-28 11:50:38 +0300

    Added csv with catalog links and h1 for spareparts

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,8 +1,11 @@
+import { convertArrayToCSV } from "convert-array-to-csv";
+const header = ["Ссылка", "H1"];
+
 const runScripts = async (strapi) => {
     let clientUrl = strapi.config.get("server.clientUrl");
-    const results = [];
+    const results = [[], []];
     const brands = await strapi.db.query("api::brand.brand").findMany({
-        populate: { models: true },
+        populate: { models: true, seoSpareParts: true },
         where: {
             spareParts: {
                 id: {
@@ -15,48 +18,64 @@ const runScripts = async (strapi) => {
         .query("api::kind-spare-part.kind-spare-part")
         .findMany();
     brands.forEach((brand) => {
-        results.push(clientUrl + "/spare-parts/" + brand.slug);
+        results[0].push(clientUrl + "/spare-parts/" + brand.slug);
+        results[1].push(
+            `${brand.seoSpareParts?.h1 || `Запчасти для ${brand.name}`}`
+        );
         brand.models.map((model) => {
-            results.push(
+            results[0].push(
                 clientUrl +
                     "/spare-parts/" +
                     brand.slug +
                     `/model-${model.slug}`
             );
+            results[1].push(
+                `${
+                    model.seoSpareParts?.h1 ||
+                    `Запчасти для ${brand.name} ${model.name}`
+                }`
+            );
             kindSpareParts.forEach((kindSparePart) => {
-                results.push(
+                results[0].push(
                     clientUrl +
                         "/spare-parts/" +
                         brand.slug +
                         `/model-${model.slug}?kindSparePart=${kindSparePart.name}`
                 );
+                results[1].push(
+                    `${`Запчасти ${kindSparePart.name} для ${brand.name} ${model.name}`}`
+                );
             });
         });
         kindSpareParts.forEach((kindSparePart) => {
-            results.push(
+            results[0].push(
                 clientUrl +
                     "/spare-parts/" +
                     brand.slug +
                     `?kindSparePart=${kindSparePart.name}`
             );
+            results[1].push(
+                `${`Запчасти ${kindSparePart.name} для ${brand.name}`}`
+            );
         });
     });
 
     kindSpareParts.forEach((kindSparePart) => {
-        results.push(
+        results[0].push(
             clientUrl + "/spare-parts" + `?kindSparePart=${kindSparePart.name}`
         );
+        results[1].push(`Запчасти ${kindSparePart.name}`);
     });
+    const csv: string = convertArrayToCSV(results, { header });
 
-    let str = results.reduce((prev, curr) => prev + curr + "\n", "");
     await strapi.plugins.email.services.email.send({
         to: "maks_zhukov_97@mail.ru",
         from: strapi.plugins.email.config("providerOptions.username"),
         subject: "Ссылки на все товары",
         attachments: [
             {
-                filename: "products.txt",
-                content: str,
+                filename: "products.csv",
+                content: "\ufeff" + csv,
             },
         ],
     });
