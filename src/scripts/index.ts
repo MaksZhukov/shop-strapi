9dfdda0e2d3360068d95f1ba91ad88eb9fc3294b
Maksim Zhukau <maksimz@checkpoint.com>
2023-05-04 12:17:54 +0300

    Fixed

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -11,8 +11,10 @@ const runScripts = async (strapi) => {
             .findMany({ populate: { seoSpareParts: true } })
     ).filter((item) => !item.seoSpareParts);
 
+
+
     await strapi.plugins.email.services.email.send({
-        to: ["maks_zhukov_97@mail.ru"],
+        to: "maks_zhukov_97@mail.ru",
         from: strapi.plugins.email.config("providerOptions.username"),
         subject: "No seoSpareParts for brands and models",
         text: JSON.stringify(brands) + " " + JSON.stringify(models),
