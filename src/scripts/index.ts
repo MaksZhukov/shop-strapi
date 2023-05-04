27d0272f2e82153ceb2ba241cd288b95fa61e71d
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-05-04 23:42:03 +0300

    Added tire brand seo fill

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,5 +1,19 @@
 const runScripts = async (strapi) => {
-
+    const tireBrands = await strapi.db
+        .query("api::tire-brand.tire-brand")
+        .findMany({ populate: { seo: true } });
+    tireBrands.forEach((item) => {
+        strapi.entityService.update("api::tire-brand.tire-brand", item.id, {
+            data: {
+                seo: {
+                    title: `Купить шины ${item.name}. Доставка. Цены не кусаются`,
+                    description: `Предлагаем купить шины ${item.name} в нашем магазине. На нашей разборке найдется все для вашего авто`,
+                    keywords: "Шины " + item.name,
+                    h1: "Шины " + item.name,
+                },
+            },
+        });
+    });
 };
 
 export default runScripts;
