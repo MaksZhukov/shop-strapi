79db46f4aa2d137577758fc2a25cece680d15191
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-05-25 00:07:51 +0300

    Added new templat for articles

---
+++ src/api/new-article/controllers/new-article.ts
@@ -0,0 +1,7 @@
+/**
+ * new-article controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::new-article.new-article');
---
+++ src/api/new-article/services/new-article.ts
@@ -0,0 +1,7 @@
+/**
+ * new-article service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::new-article.new-article');
