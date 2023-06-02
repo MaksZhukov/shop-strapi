c9e72871a70f1e36f0a3cd7edf6c3ffd805a7b5e
Maksim Zhukau <maksimz@checkpoint.com>
2023-06-02 20:42:04 +0300

    Removed tracking id and added query for notification

--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -4,7 +4,6 @@
 
 import { factories } from "@strapi/strapi";
 import { checkout } from "../../../services/bepaid";
-import { decrypt, encrypt } from "../../../services/crypto";
 
 const PRODUCT_API_UID_BY_TYPE = {
     cabin: "api::cabin.cabin",
@@ -33,14 +32,13 @@ export default factories.createCoreController(
             if (productsEntities.some((item) => item.sold)) {
                 return ctx.badRequest("product is sold");
             }
-            const trackingId = encrypt(JSON.stringify(products));
             const data = await checkout(
                 productsEntities.map((item) => item.h1).join(", "),
                 productsEntities.reduce(
                     (prev, curr) => prev + (curr.discountPrice || curr.price),
                     0
                 ),
-                trackingId
+                { products }
             );
             return { data };
         },
@@ -53,6 +51,7 @@ export default factories.createCoreController(
                 customer,
                 billing_address,
             } = ctx.request.body.transaction || {};
+            const { products } = ctx.query;
             if (status === "successful") {
                 const order = await strapi.db
                     .query("api::order.order")
@@ -62,7 +61,6 @@ export default factories.createCoreController(
                         },
                     });
                 if (!order) {
-                    const products = decrypt(trackingId);
                     const entry = await strapi.entityService.create(
                         "api::order.order",
                         {
