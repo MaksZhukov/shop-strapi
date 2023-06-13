929f1f1bd210b090b586f10442764691d209ff73
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-14 00:03:21 +0300

    Refactored sending url of products

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,25 +1,3 @@
-import { productTypeUrlSlug } from "../config";
-import { runProductsQueriesWithLimit } from "../services";
-import { updateImageMetadata } from "../services/imageMetadata";
-
-const runScripts = async (strapi) => {
-    let clientUrl = strapi.config.get("server.clientUrl");
-    let queries = [
-        strapi.db.query("api::spare-part.spare-part"),
-        strapi.db.query("api::cabin.cabin"),
-        strapi.db.query("api::wheel.wheel"),
-        strapi.db.query("api::tire.tire"),
-    ];
-    runProductsQueriesWithLimit(queries, 100, (data) => {
-        data.forEach((item) => {
-            item.images?.forEach((image) => {
-                updateImageMetadata(
-                    image.url,
-                    `${clientUrl}/${productTypeUrlSlug[item.type]}/${item.slug}`
-                );
-            });
-        });
-    });
-};
+const runScripts = async (strapi) => {};
 
 export default runScripts;
