0e5c3c7ec0c94e61d4c49aa3e4a72c8b7fb999d1
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-13 08:28:51 +0300

    Added schedule update metadata for image after create and update

--- src/api/cabin/content-types/cabin/lifecycles.ts
+++ src/api/cabin/content-types/cabin/lifecycles.ts
@@ -3,6 +3,7 @@ import {
     removeFavoritesOnSold,
     scheduleUpdateAltTextForProductImages,
 } from "../../../../services";
+import { scheduleUpdateImageMetadata } from "../../../../services/imageMetadata";
 
 // import { afterDeleteProduct } from "../../../../lifecycles";
 
@@ -10,11 +11,13 @@ export default {
     beforeCreate: beforeCreateProduct,
     afterCreate: (data) => {
         scheduleUpdateAltTextForProductImages(data.result, "api::cabin.cabin");
+        scheduleUpdateImageMetadata(data.result, "api::cabin.cabin");
         lifecycleSitemap();
     },
     afterUpdate: (data) => {
         scheduleUpdateAltTextForProductImages(data.result, "api::cabin.cabin");
         removeFavoritesOnSold(data, "product.cabin");
+        scheduleUpdateImageMetadata(data.result, "api::cabin.cabin");
         lifecycleSitemap();
     },
     afterDelete: lifecycleSitemap,
--- src/api/tire/content-types/tire/lifecycles.ts
+++ src/api/tire/content-types/tire/lifecycles.ts
@@ -1,15 +1,21 @@
 import { beforeCreateProduct, lifecycleSitemap } from "../../../../lifecycles";
-import { removeFavoritesOnSold, scheduleUpdateAltTextForProductImages } from "../../../../services";
+import {
+    removeFavoritesOnSold,
+    scheduleUpdateAltTextForProductImages,
+} from "../../../../services";
+import { scheduleUpdateImageMetadata } from "../../../../services/imageMetadata";
 
 export default {
     beforeCreate: beforeCreateProduct,
     afterCreate: (data) => {
         scheduleUpdateAltTextForProductImages(data.result, "api:tire.tire");
+        scheduleUpdateImageMetadata(data.result, "api::tire.tire");
         lifecycleSitemap();
     },
     afterUpdate: (data) => {
         scheduleUpdateAltTextForProductImages(data.result, "api::tire.tire");
         removeFavoritesOnSold(data, "product.tire");
+        scheduleUpdateImageMetadata(data.result, "api::tire.tire");
         lifecycleSitemap();
     },
     afterDelete: lifecycleSitemap,
