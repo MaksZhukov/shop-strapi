d3504a29060bc5a7a7fcd09a753ccaf3df1c8549
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-18 09:06:43 +0300

    Optimize code

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -3,14 +3,14 @@ import {
     sendProductsInCSVToEmail,
     updateCurrency,
 } from "../src/services";
-import { sendYMLToEmail } from "../src/services/yml/yml";
+import { sendYMLsToEmail } from "../src/services/yml/yml";
 
 export default {
     "0 9 * * *": sendNewProductsToEmail,
     "1 9 * * *": sendProductsInCSVToEmail,
-    "2 9 * * *": sendYMLToEmail,
+    "2 9 * * *": sendYMLsToEmail,
     "3 9 * * *": () => {
         strapi.service("plugin::telegram.service")?.runTelegramMessages();
     },
-    "0 */12 * * *": updateCurrency
+    "0 */12 * * *": updateCurrency,
 };
--- src/services/yml/config.ts
+++ src/services/yml/config.ts
@@ -1,10 +1,29 @@
 import { getProductUrl } from "..";
-import { CATEGORIES, CATEGORIES_TEXTS } from "./constants";
+import { CATEGORIES } from "./constants";
+
+export const templateOffers = (products: any[]) => {
+    const serverUrl = strapi.config.get("server.serverUrl");
+    const clientUrl = strapi.config.get("server.clientUrl");
+    return products
+        .map(
+            (item) => `\t<offer available="true" id="${
+                item.id + "-" + CATEGORIES.findIndex((cat) => cat === item.type)
+            }">
+    \t<name>${item.h1}</name>
+    \t<price>${item.price}</price>     
+    \t<picture>${
+        item.images ? serverUrl + item.images[0].url : clientUrl + "/logo.jpg"
+    }</picture>               
+    \t<description>${item.description} ${getProductUrl(
+                item
+            )}</description>                           
+\t</offer>\n\t`
+        )
+        .join("");
+};
 
 export const template = (
-    products: any[],
-    serverUrl: string,
-    clientUrl: string
+    offers: string
 ) => `<?xml version="1.0" encoding="UTF-8"?>
 <yml_catalog date="${new Date().toISOString()}">
     <shop>
@@ -12,26 +31,7 @@ export const template = (
         <company>Razbor auto</company>
         <url>https://razbor-auto.by/</url>
         <offers>
-        ${products
-            .map(
-                (item) => `\t<offer available="true" id="${
-                    item.id +
-                    "-" +
-                    CATEGORIES.findIndex((cat) => cat === item.type)
-                }">
-                \t<name>${item.h1}</name>
-                \t<price>${item.price}</price>     
-                \t<picture>${
-                    item.images
-                        ? serverUrl + item.images[0].url
-                        : clientUrl + "/logo.jpg"
-                }</picture>               
-                \t<description>${
-                    item.description
-                } ${getProductUrl(item)}</description>                           
-            \t</offer>\n\t`
-            )
-            .join("")}
+        ${offers}
         </offers>
     </shop>
 </yml_catalog>`;
