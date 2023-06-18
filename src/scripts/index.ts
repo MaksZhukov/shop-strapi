9180642340b6336284ce3536dd5cbe8b8fdfeea9
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-19 00:08:53 +0300

    Removed script

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,12 +1,3 @@
-import { updateAltTextForProductImages } from "../services";
-
-const runScripts = async (strapi) => {
-    const tires = await strapi.db
-        .query("api::tire.tire")
-        .findMany({ populate: ["images"] });
-    tires.forEach((item) => {
-        updateAltTextForProductImages(item, item.images);
-    });
-};
+const runScripts = async (strapi) => {};
 
 export default runScripts;
