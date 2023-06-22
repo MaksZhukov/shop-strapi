480064a596904540383215a769887b812bbccbe3
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-06-22 22:19:55 +0300

    Added script for changing metadata

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,3 +1,65 @@
-const runScripts = async (strapi) => {};
+import { productTypeUrlSlug } from "../config";
+import { updateImageMetadata } from "../services/imageMetadata";
+
+const daysInMilliseconds = 86400000 * 1;
+
+const runScripts = async (strapi) => {
+    let clientUrl = strapi.config.get("server.clientUrl");
+    const spareParts = await strapi.db
+        .query("api::spare-part.spare-part")
+        .findMany({
+            where: {
+                $or: [
+                    {
+                        updatedAt: {
+                            $gt: new Date().getTime() - daysInMilliseconds,
+                        },
+                    },
+                    {
+                        createdAt: {
+                            $gt: new Date().getTime() - daysInMilliseconds,
+                        },
+                    },
+                ],
+            },
+            populate: ["images"],
+        });
+
+    spareParts.forEach((item) => {
+        item.images?.forEach((image) => {
+            updateImageMetadata(
+                image.url,
+                `${clientUrl}/${productTypeUrlSlug[item.type]}/${item.slug}`
+            );
+        });
+    });
+
+    const cabins = await strapi.db.query("api::cabin.cabin").findMany({
+        where: {
+            $or: [
+                {
+                    updatedAt: {
+                        $gt: new Date().getTime() - daysInMilliseconds,
+                    },
+                },
+                {
+                    createdAt: {
+                        $gt: new Date().getTime() - daysInMilliseconds,
+                    },
+                },
+            ],
+        },
+        populate: ["images"],
+    });
+
+    cabins.forEach((item) => {
+        item.images?.forEach((image) => {
+            updateImageMetadata(
+                image.url,
+                `${clientUrl}/${productTypeUrlSlug[item.type]}/${item.slug}`
+            );
+        });
+    });
+};
 
 export default runScripts;
