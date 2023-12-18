1feb3425148072e1e788b8d679da4aed1bbc2ed5
Maksim Zhukau <maks_zhukov_97@mail.ru>
2023-12-19 00:29:03 +0300

    Commented sending data to emails

--- config/cron-tasks.ts
+++ config/cron-tasks.ts
@@ -6,9 +6,9 @@ import {
 import { sendYMLsToEmail } from "../src/services/yml/yml";
 
 export default {
-    "0 9 * * *": sendNewProductsToEmail,
-    "1 9 * * *": sendProductsInCSVToEmail,
-    "2 9 * * *": sendYMLsToEmail,
+    // "0 9 * * *": sendNewProductsToEmail,
+    // "1 9 * * *": sendProductsInCSVToEmail,
+    // "2 9 * * *": sendYMLsToEmail,
     "3 9 * * *": () => {
         strapi.service("plugin::telegram.service")?.runTelegramMessages();
     },
