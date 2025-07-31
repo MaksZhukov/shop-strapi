1f73909da4a61501be0262d2388f18bd44725e64
Maksim Zhukau <maksim.zhukau@ox.security>
2025-08-01 00:45:46 +0200

    Updated 1 week slug h1

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import slugify from "slugify";
 import runScripts from "./scripts";
 import {
     generateProductFullDescription,
+    getProductH1,
     hasDelayOfProductDescriptionGenerated,
     hasDelayOfSendingNewProductsEmail,
     hasDelayOfSendingProductsInCsvEmail,
@@ -34,6 +36,79 @@ export default {
         if (process.env.NODE_ENV === "production") {
             sendNotificationOnStart();
             if (process.env.NODE_APP_INSTANCE === "0") {
+                const oneWeekAgo = new Date();
+                oneWeekAgo.setDate(oneWeekAgo.getDate() - 8);
+
+                // Query cabins created or updated in the last week
+                const cabins = await strapi.db
+                    .query("api::cabin.cabin")
+                    .findMany({
+                        populate: ["brand", "model", "generation", "images"],
+                        where: {
+                            $or: [
+                                {
+                                    createdAt: { $gte: oneWeekAgo },
+                                },
+                            ],
+                        },
+                        orderBy: { createdAt: "desc" },
+                    });
+                cabins.forEach(async (cabin) => {
+                    const h1 =
+                        cabin.name +
+                        " " +
+                        (cabin.brand?.name ?? "") +
+                        " " +
+                        (cabin.model?.name ?? "");
+                    const slug =
+                        slugify(cabin.name, { lower: true, strict: true }) +
+                        "-" +
+                        cabin.id;
+                    const updatedCabin = await strapi.db
+                        .query("api::cabin.cabin")
+                        .update({
+                            where: { id: cabin.id },
+                            data: {
+                                h1: h1,
+                                slug: slug,
+                            },
+                        });
+                });
+
+                // Query cabins created or updated in the last week
+                const spareParts = await strapi.db
+                    .query("api::spare-part.spare-part")
+                    .findMany({
+                        populate: ["brand", "model", "generation", "images"],
+                        where: {
+                            $or: [
+                                {
+                                    createdAt: { $gte: oneWeekAgo },
+                                },
+                            ],
+                        },
+                        orderBy: { createdAt: "desc" },
+                    });
+                spareParts.forEach(async (sparePart) => {
+                    const h1 =
+                        sparePart.name +
+                        " " +
+                        (sparePart.brand?.name ?? "") +
+                        " " +
+                        (sparePart.model?.name ?? "");
+                    const slug =
+                        slugify(sparePart.name, { lower: true, strict: true }) +
+                        "-" +
+                        sparePart.id;
+
+                    await strapi.db.query("api::spare-part.spare-part").update({
+                        where: { id: sparePart.id },
+                        data: {
+                            h1: h1,
+                            slug: slug,
+                        },
+                    });
+                });
                 scheduleGenerateSitemap();
                 // if (await hasDelayOfSendingNewProductsEmail(strapi)) {
                 //     sendNewProductsToEmail({ strapi });
