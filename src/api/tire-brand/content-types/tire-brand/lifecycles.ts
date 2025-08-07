02470b40a36bcf2588132b9a1535246989b90cc9
Maks Zhukov <maks_zhukov_97@mail.ru>
2025-08-07 17:22:01 +0200

    Did the code field is optional and added generation code from it for new entities

--- src/api/tire-brand/content-types/tire-brand/lifecycles.ts
+++ src/api/tire-brand/content-types/tire-brand/lifecycles.ts
@@ -8,6 +8,9 @@ import {
 export default {
     beforeCreate(event) {
         const { data } = event.params;
+        if (data.id && !data.code) {
+            data.code = data.id;
+        }
         if (data.name) {
             data.slug = slugify(data.name, { lower: true, strict: true });
         }
