8894a466198396f8beccb1dc7ee676aac1a0646c
Maksim Zhukau <maksim.zhukau@ox.security>
2025-08-07 12:38:39 +0200

    Added migration id to code

--- database/migrations/2023.05.02T00.00.00.set-desc-spare-parts-default.js
+++
@@ -1,9 +0,0 @@
-async function up(knex) {
-    console.log("hello");
-    await knex("spare_parts").where({ description: null }).update({
-        description:
-            "Оригинальная запчасть, которая прошла тщательную проверку перед продажей. Она подходит для данной модели авто. Товар находится в хорошем состоянии и готов к установке. В хорошем состоянии. Из Европы. Доставка",
-    });
-}
-
-module.exports = { up };
